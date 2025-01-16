ab7dd220be7d7017958e397593c942e3c8ae14c0
Michael Puckett <michaelcpuckett@gmail.com>
2025-01-16 12:34:53 -0500

    cleanup install script

--- dist/install.js
+++ dist/install.js
@@ -1,30 +1,31 @@
-window.addEventListener('DOMContentLoaded', () => {
-  window.navigator.serviceWorker
-    .register('/app.js')
-    .then(async (registration) => {
-      const serviceWorker =
-        registration.installing || registration.waiting || registration.active;
+window.navigator.serviceWorker
+  .register('/app.js')
+  .catch((error) => {
+    throw new Error('Service worker registration failed: ' + error);
+  })
+  .then(async (registration) => {
+    const serviceWorker =
+      registration.installing || registration.waiting || registration.active;
 
-      if (!serviceWorker) {
-        throw new Error('Service worker not found.');
+    if (!serviceWorker) {
+      throw new Error('Service worker not found.');
+    }
+
+    // Wait for service worker to activate.
+    await new Promise((resolve) => {
+      if (serviceWorker.state === 'activated') {
+        resolve();
+      } else {
+        serviceWorker.addEventListener('statechange', (event) => {
+          if (event.target.state === 'activated') {
+            resolve();
+          }
+        });
       }
-
-      // Wait for service worker to activate.
-      await new Promise((resolve) => {
-        if (serviceWorker.state === 'activated') {
-          resolve();
-        } else {
-          serviceWorker.addEventListener('statechange', (e) => {
-            if (e.target.state === 'activated') {
-              resolve();
-            }
-          });
-        }
-      });
-
-      window.location.reload();
-    })
-    .catch((error) => {
-      console.error(error);
     });
-});
+
+    window.location.reload();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
