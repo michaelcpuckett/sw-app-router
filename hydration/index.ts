3fe72916f0490a2f2176decad046374afd500df8
Michael Puckett <michaelcpuckett@gmail.com>
2025-01-19 17:23:20 -0500

    Upgrade to latest version of @express-worker/router

--- hydration/index.ts
+++
@@ -1,4 +0,0 @@
-import routesConfig from '.express-worker-router/routes';
-import { useHydration } from '@express-worker/router/hydration';
-
-useHydration({ routesConfig });
--- service-worker/index.ts
+++
@@ -1,6 +0,0 @@
-import { version } from '.express-worker-router/cache.json';
-import routesConfig from '.express-worker-router/routes';
-import staticFiles from '.express-worker-router/static.json';
-import { useAppRouter } from '@express-worker/router/service-worker';
-
-useAppRouter({ routesConfig, staticFiles, version });
